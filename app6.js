dc69583f2a80ccf528efd00fe11421aa28e3f206
AntonioRivera2004 <antonio.riverarz@hotmail.com>
2025-09-01 22:07:44 -0600

    Primer Commit

---
+++ app6.js
@@ -0,0 +1,17 @@
+let A=parseFloat(prompt("Ingresa el numero A: "));
+let B=parseFloat(prompt("Ingresa el numero B: "));
+
+let suma, resta, multiplicacion, division, modulo;
+
+suma=A+B;
+resta=A-B;
+multiplicacion=A*B;
+division=A/B
+modulo=A%B;
+
+console.log("Los resultados son: \n");
+console.log("Suma= "+suma);
+console.log("Resta= "+resta);
+console.log("Multiplicación= "+multiplicacion);
+console.log("Division= "+division);
+console.log("Modulo= "+modulo);
